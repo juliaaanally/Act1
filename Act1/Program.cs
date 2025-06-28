a780f31fda8a631553cad6472caec600fa85aa1d
juliaaanally <julianaallysonelardo@gmail.com>
2025-06-28 16:02:19 +0800

    created branch converter

--- Act1/Program.cs
+++ Act1/Program.cs
@@ -13,5 +13,31 @@ class UnitConverter
 
         Console.WriteLine();
 
+        double result = 0.0;
+
+        if (choice == 1)
+        {
+            Console.Write("Enter distance in kilometers: ");
+            double km = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+            result = km * 0.621371;
+            Console.WriteLine($"{km} km is {result:F2} miles.\n");
+        }
+        else if (choice == 2)
+        {
+            Console.Write("Enter distance in miles: ");
+            double miles = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+            result = miles / 0.621371;
+            Console.WriteLine($"{miles} miles is {result:F2} kilometers.\n");
+        }
+        else
+        {
+            Console.WriteLine("Invalid choice!");
+        }
+
+        Console.WriteLine("Done. Press any key to exit.");
+        Console.ReadKey();
+
     }
 }
