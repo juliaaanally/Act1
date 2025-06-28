92cf89b9a68d1ffceff1a804646ae91fbabb31d7
juliaaanally <julianaallysonelardo@gmail.com>
2025-06-28 15:55:25 +0800

    created main

--- Act1/Program.cs
+++ Act1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class UnitConverter
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== Unit Converter ===\n");
+        Console.WriteLine("1. Kilometers to Miles");
+        Console.WriteLine("2. Miles to Kilometers\n");
+        Console.Write("Choose an option (1 or 2): ");
+
+        int choice = int.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+
+    }
+}
